1c3d193842130c80c031e6890068c0671f005b83
Lucas Abreu <lucasabreu.dev@gmail.com>
2023-01-12 22:03:19 -0300

    fix: ajustes na CurrentAccount

--- bytebank/Account/CurrentAccount.cs
+++ bytebank/Account/CurrentAccount.cs
@@ -9,29 +9,27 @@ namespace bytebank.Account
 {
     public class CurrentAccount
     {
-        private long bankBranch;
-        public long BankBranch
-        {
-            get { return this.bankBranch; }
-            private set
-            {
-                if (value > 0)
-                {
-                    this.bankBranch = value;
-                }
-            }
-
-        }
-        public string Account { get; set; }
         public AccountHolder AccountHolder { get; set; }
+        public long BankBranch { get; }
+        public string AccountNumber { get; }
         public double Balance { get; private set; }
+
+        public double OperationRate { get; private set; }
 
         public static long TotalAccounts { get; private set; }
 
-        public CurrentAccount(long bankBranch, string account)
+        public CurrentAccount(long bankBranch, string accountNumber)
         {
+            if (bankBranch == 0)
+                throw new ArgumentException("Agência não pode ser igual a 0.", nameof(bankBranch));
+
+            if (accountNumber.Equals("0"))
+                throw new ArgumentException("Número da conta não pode ser igual a 0.", nameof(accountNumber));
+
             this.BankBranch = bankBranch;
-            this.Account = account;
+            this.AccountNumber = accountNumber;
+
+            OperationRate = 100 / TotalAccounts;
             TotalAccounts++;
         }
 
@@ -61,17 +59,17 @@ namespace bytebank.Account
             return false;
         }
 
-        public bool MoneyTransfer(double value, CurrentAccount destination)
+        public void MoneyTransfer(double value, CurrentAccount destination)
         {
             VerifyValue(value);
 
             if (Balance < value)
             {
-                return false;
+                throw new Exception("Saldo insuficiente.");
             }
             Withdraw(value);
             destination.Deposit(value);
-            return true;
+
         }
     }
 }
